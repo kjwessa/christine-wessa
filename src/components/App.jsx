eb7de40aeee65f63e438a63cd29427d07bd5cc41
Kevin Wessa <kjwessa@gmail.com>
2024-04-12 11:59:51 -0400

    feat(Footer): add initial Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col text-slate-900">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
