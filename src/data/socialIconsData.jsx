8c48dbc019f3ffaa3da18c9a71f2a32d16dd122e
Kevin Wessa <kjwessa@gmail.com>
2024-04-12 12:30:20 -0400

    feat: add data for social icons

---
+++ src/data/socialIconsData.jsx
@@ -0,0 +1,9 @@
+const socialIconsData = [
+  { id: 1, name: "Instagram", url: "", svg: "" },
+  { id: 2, name: "Facebook", url: "", svg: "" },
+  { id: 3, name: "YouTube", url: "", svg: "" },
+  { id: 4, name: "LinkedIn", url: "", svg: "" },
+  { id: 5, name: "Email", url: "", svg: "" },
+];
+
+export default socialIconsData;
