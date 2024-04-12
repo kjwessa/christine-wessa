8b171bfc601ef49b0dab5948a06a3d593db0faa2
Kevin Wessa <kjwessa@gmail.com>
2024-04-12 12:36:01 -0400

    feat: add urls for social icons

--- src/data/socialIconsData.jsx
+++ src/data/socialIconsData.jsx
@@ -1,9 +1,29 @@
 const socialIconsData = [
-  { id: 1, name: "Instagram", url: "", svg: "" },
-  { id: 2, name: "Facebook", url: "", svg: "" },
-  { id: 3, name: "YouTube", url: "", svg: "" },
-  { id: 4, name: "LinkedIn", url: "", svg: "" },
-  { id: 5, name: "Email", url: "", svg: "" },
+  {
+    id: 1,
+    name: "Instagram",
+    url: "https://www.instagram.com/christinewessa/",
+    svg: "",
+  },
+  {
+    id: 2,
+    name: "Facebook",
+    url: "https://www.facebook.com/christinesimpson.wessa.7",
+    svg: "",
+  },
+  {
+    id: 3,
+    name: "YouTube",
+    url: "https://www.youtube.com/@hisownmusic7174",
+    svg: "",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/christine-wessa-8237501b4/",
+    svg: "",
+  },
+  { id: 5, name: "Email", url: "mailto: ${REACT_APP_EMAIL_ADDRESS}", svg: "" },
 ];
 
 export default socialIconsData;
