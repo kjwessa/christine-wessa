b35a2e6f0285e5c4e748404036f1a74186127e76
Kevin Wessa <kjwessa@gmail.com>
2024-04-12 11:40:11 -0400

    feat(Header): add Header component with basic Tailwind styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="flex items-center justify-center py-3">
+      <div>
+        <h1 className="font-display text-1xl font-normal">Christine Wessa</h1>
+      </div>
+    </header>
+  );
+}
