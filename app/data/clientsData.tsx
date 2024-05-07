69c05dff080b5f97363cfb8fd1bc3c3f4bb0800f
Kevin Wessa <kjwessa@gmail.com>
2024-05-07 09:28:17 -0400

    chores(clientsData): add initial list of clients

---
+++ app/data/clientsData.tsx
@@ -0,0 +1,74 @@
+const clientsData = [
+  {
+    id: 1,
+    name: "Maine Catholic Women's Conference",
+    location: "Portland, ME",
+    logo: "",
+  },
+  {
+    id: 2,
+    name: "Austin Catholic Women's Conference",
+    location: "Austin, TX",
+    logo: "",
+  },
+  {
+    id: 3,
+    name: "Scranton Catholic Women's Conference",
+    location: "Scranton, PA",
+    logo: "",
+  },
+  {
+    id: 4,
+    name: "Columbus Catholic Women's Conference",
+    location: "Columbus, OH",
+    logo: "",
+  },
+  {
+    id: 5,
+    name: "Youth2000 - Diocese of Owensboro, KY",
+    location: "Owensboro, KY",
+    logo: "",
+  },
+  {
+    id: 6,
+    name: "Catholic Underground",
+    location: "New York",
+    logo: "",
+  },
+  {
+    id: 7,
+    name: "Love & Responsibility",
+    location: "New York",
+    logo: "",
+  },
+  {
+    id: 8,
+    name: "St. John Vianney",
+    location: "Atlanta, GA",
+    logo: "",
+  },
+  {
+    id: 8,
+    name: "St. Dominic",
+    location: "Panama City, FL",
+    logo: "",
+  },
+  {
+    id: 9,
+    name: "Regnum Chrisit",
+    location: "Nashville, TN",
+    logo: "",
+  },
+  {
+    id: 10,
+    name: "Sacred Heart Cathedral",
+    location: "Pensacola, FL",
+    logo: "",
+  },
+  {
+    id: 11,
+    name: "St. Patrick's",
+    location: "Troy, OH",
+    logo: "",
+  },
+];
