de3bea6ad8839604b2f7ad08c76c1c404ef5c291
Kevin Wessa <kjwessa@gmail.com>
2024-05-07 09:43:19 -0400

    refactor(HomeLanding): move landing page code to separate component for reusability

---
+++ app/components/HomeLanding.tsx
@@ -0,0 +1,16 @@
+import SocialIconsList from "./SocialIconsList";
+
+export default function HomeLanding() {
+  return (
+    <section className="flex flex-auto flex-col items-center justify-center">
+      <h2 className="text-5xl">Christine Wessa</h2>
+      <SocialIconsList filter={["Instagram", "Facebook", "YouTube", "Email"]} />
+      <p className="max-w-80 text-balance pt-2  text-center text-sm">
+        To inquire about booking, send me a message at{" "}
+        <a href="mailto:bookchristinewessa@gmail.com" className="font-bold">
+          bookchristinewessa@gmail.com
+        </a>
+      </p>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-import SocialIconsList from "./components/SocialIconsList";
+import HomeLanding from "./components/HomeLanding";
 export default function Page() {
-  return (
-    <section className="flex flex-auto flex-col items-center justify-center">
-      <h2 className="text-5xl">Christine Wessa</h2>
-      <SocialIconsList filter={["Instagram", "Facebook", "YouTube", "Email"]} />
-      <p className="max-w-80 text-balance pt-2  text-center text-sm">
-        To inquire about booking, send me a message at{" "}
-        <a href="mailto:bookchristinewessa@gmail.com" className="font-bold">
-          bookchristinewessa@gmail.com
-        </a>
-      </p>
-    </section>
-  );
+  return <HomeLanding />;
 }
