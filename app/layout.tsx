b65e8ac9a625699435d398c5ed70282f3c55ec00
Kevin Wessa <kjwessa@gmail.com>
2024-05-08 10:22:06 -0400

    feat(SpeedInsights): add Vercel Speed Insights to better understand user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -22,6 +23,7 @@ export default function RootLayout({
           {children}
           <Footer />
           <Analytics />
+          <SpeedInsights />
         </main>
       </body>
     </html>
