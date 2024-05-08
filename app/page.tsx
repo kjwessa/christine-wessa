173577550a7683bbe5dfba6b7ea23d57649b2e57
Kevin Wessa <kjwessa@gmail.com>
2024-05-08 08:58:29 -0400

    feat(HomeHero): add homepage hero component introducing Christine

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { HomeLanding } from "./components/HomeLanding";
+import { HomeHero } from "./components/HomeHero";
 export default function Page() {
-  return <HomeLanding />;
+  return <HomeHero />;
 }
