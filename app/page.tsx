a8e7565bc8e2be77463236c77663053930ddd296
Kevin Wessa <kjwessa@gmail.com>
2024-05-08 10:40:57 -0400

    feat(next-font): utilize Next local fonts for optimized loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { HomeLanding } from "./components/HomeLanding";
+import { HomeHero } from "./components/HomeHero";
 
 export default function Page() {
-  return <HomeLanding />;
+  return <HomeHero />;
 }
