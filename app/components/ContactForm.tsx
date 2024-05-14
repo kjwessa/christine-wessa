733041554953f4cd7a2fbbce3f06853f3abedb5b
Kevin Wessa <kjwessa@gmail.com>
2024-05-14 11:14:46 -0400

    feat(forms): add new fields to capture better inquiries

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -3,27 +3,52 @@ export function ContactForm() {
   return (
     <div>
       <form action={formActionUrl} method="POST" target="_blank">
+        <label>First Name</label>
         <input
           type="text"
           name="entry.878151725"
-          placeholder="Your Name"
+          placeholder="First Name"
           required
         />
+        <label>Last Name</label>
+        <input
+          type="text"
+          name="entry.947208125"
+          placeholder="Last Name"
+          required
+        />
+        <label>Contact Email</label>
         <input
           type="email"
           name="entry.215650103"
-          placeholder="Your Email"
+          placeholder="name@example.com"
           required
         />
+        <label>Phone Number</label>
         <input
           type="tel"
           name="entry.1320803298"
-          placeholder="Your Phone Number"
+          placeholder="e.g. (123) 456-7890"
         />
+        <label>Organization/Parish</label>
+        <input
+          type="text"
+          name="entry.1714866494"
+          placeholder="Austin Women's Conference"
+          required
+        />
+        <label>Location</label>
+        <input
+          type="text"
+          name="entry.441226641"
+          placeholder="Austin, TX"
+          required
+        />
+        <label>Your Message</label>
         <input
           type="textarea"
           name="entry.94188901"
-          placeholder="Your Message"
+          placeholder="Please provide as much information as possible about your upcoming event."
           required
         />
         <button type="submit" value="Submit">
