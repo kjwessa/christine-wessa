0febaec37242bfaad162a61a02103bc8bf3d1fef
Kevin Wessa <kjwessa@gmail.com>
2024-05-14 12:50:03 -0400

    feat(data): add testimonial for Fr. Jack Knight

--- app/data/testimonialsData.tsx
+++ app/data/testimonialsData.tsx
@@ -2,6 +2,7 @@ export const testimonialsData = [
   {
     id: 1,
     person: "Fr. Mary Mary, CFR",
+    avatar: "",
     role: "CFR Brooklyn",
     diocese: "",
     testimonial:
@@ -9,30 +10,20 @@ export const testimonialsData = [
   },
   {
     id: 2,
-    person: "Fr. Jack Knight",
+    person: "Very Rev. Fr. John Phillip Knight, V.F.",
+    avatar: "",
     role: "Pastor of St. John Vianney",
     diocese: "Archdiocese of Atlanta",
-    testimonial: "",
+    testimonial:
+      "Christine Wessa is an incredible gift to the Church and she helps facilitate an encounter God through music and speaking.  When she came to do our parish mission, she provided nothing short of a foretaste of heaven.  If you ever get the opportunity to bring her to your community, do not hesitate.",
   },
   {
     id: 3,
     person: "Chez Filipini",
+    avatar: "",
     role: "Director of the Office of Marriage and Family Life",
     diocese: "Diocese of Pensacola-Tallahassee",
-    testimonial: "Christine is a uniquely gifted worship leader whose demeanor and musical excellence work together to foster beautiful moments of prayerfully encountering the Lord Jesus. She is capable of encouraging this spirit with different age groups who might have varying levels of experience or comfort in retreat settings. She is a joy to work with and displays a servant's heart in every planning conversation and email exchange.",
-  },
-  {
-    id: 4,
-    person: "Fr. Jacob Jaks",
-    role: "",
-    diocese: "Diocese of Pensacola-Tallahassee",
-    testimonial: "",
-  },
-  {
-    id: 5,
-    person: "Fr. Eric Bowman",
-    diocese: "",
-    role: "",
-    testimonial: "",
+    testimonial:
+      "Christine is a uniquely gifted worship leader whose demeanor and musical excellence work together to foster beautiful moments of prayerfully encountering the Lord Jesus. She is capable of encouraging this spirit with different age groups who might have varying levels of experience or comfort in retreat settings. She is a joy to work with and displays a servant's heart in every planning conversation and email exchange.",
   },
 ];
