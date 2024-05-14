9242dd9259c0a95431b1b5db89472ed103c43505
Kevin Wessa <kjwessa@gmail.com>
2024-05-14 11:44:44 -0400

    feat(ui): add initial styling to ContactForm

--- app/components/Home/HomeContact.tsx
+++ app/components/Home/HomeContact.tsx
@@ -3,7 +3,10 @@ import { ContactForm } from "../ContactForm";
 export function HomeContact() {
   return (
     <section className="border-b-1 border-slate-200 py-24">
-      <div className="mx-auto grid grid-cols-2 grid-rows-2 gap-4 px-2 md:grid-cols-[1fr_auto_1fr] md:grid-rows-1">
+      <div className="px-2">
+        <h2 className="text-4xl">
+          Fill out the form below and I will be in touch within 48 hrs.{" "}
+        </h2>
         <ContactForm />
       </div>
     </section>
