f25add16b14cdd35e30878ed483772a736dc761f
Kevin Wessa <kjwessa@gmail.com>
2024-05-14 16:50:08 -0400

    feat(ui): add Instagram icon, overlay, and link to InstagramImage

--- app/components/InstagramImage.tsx
+++ app/components/InstagramImage.tsx
@@ -1,17 +1,45 @@
 import Image from "next/image";
+import socialIconsData from "../data/socialIconsData";
+import Link from "next/link";
 
 interface InstagramImageProps {
   src: string;
   alt: string;
+  overlay?: boolean;
 }
 
-export function InstagramImage({ src, alt }: InstagramImageProps) {
+interface socialIconsDataProps {
+  link: string;
+}
+
+export function InstagramImage({ src, alt, overlay }: InstagramImageProps) {
+  // Get Instagram data object
+  const instagramData = socialIconsData.find(
+    (link) => link.name === "Instagram",
+  );
+
+  // Isolate Instagram link
+  const instagramLink =
+    instagramData?.url ?? "https://www.instagram.com/christinewessa/";
+
+  // Isolate Instagram SVG
+  const instagramIcon = instagramData?.svg;
+
   return (
-    <div
-      className="relative h-full w-full flex-grow overflow-hidden rounded-md"
+    <Link
+      href={instagramLink}
+      className="font relative h-full w-full flex-grow overflow-hidden rounded-md font-bold text-white"
       style={{ paddingTop: "100%" }}
     >
       <Image src={src} alt={alt} className="object-cover" fill />
-    </div>
+      {overlay && (
+        <div className="absolute left-0 top-0 z-10 flex h-full w-full flex-col items-center justify-center ">
+          <div className="flex flex-col items-center">
+            <span>{instagramIcon}</span>
+            <span>@ChristineWessa</span>
+          </div>
+        </div>
+      )}
+    </Link>
   );
 }
