2c4c4472de23bf3548773d28f6f6cf607db7d931
Kevin Wessa <kjwessa@gmail.com>
2024-05-14 18:29:48 -0400

    feat(ui): update images in InstagramImageList

--- app/components/InstagramImageList.tsx
+++ app/components/InstagramImageList.tsx
@@ -4,25 +4,25 @@ export function InstagramImageList() {
   return (
     <div className="mx-auto grid w-full max-w-7xl grid-cols-5 gap-2 ">
       <InstagramImage
-        src="/images/christineWessa_pensacola_walking-across-street.jpg"
-        alt=""
+        src="/images/insta/IMG_4216.jpeg"
+        alt="Kevin Wessa and Christine Simpson Wessa outside at Sacred Heart Cathedral in Pensacola, FL"
       />
       <InstagramImage
-        src="/images/christineWessa_pensacola_walking-across-street.jpg"
-        alt=""
+        src="/images/insta/IMG_5149.jpeg"
+        alt="Kevin Wessa and Christine Simpson Wessa after the Christmas Vigil Mass at Sacred Heart Cathedral in Pensacola, FL"
       />
       <InstagramImage
-        src="/images/christineWessa_pensacola_walking-across-street.jpg"
-        alt=""
+        src="/images/insta/IMG_5915.jpeg"
+        alt="Little Ava at the Youth 2000 event in Owensboro, KY"
         overlay={true}
       />
       <InstagramImage
-        src="/images/christineWessa_pensacola_walking-across-street.jpg"
-        alt=""
+        src="/images/insta/IMG_5953.jpeg"
+        alt="Maria Spears and Christine Simpson Wessa backstage at the Youth 2000 event in Owensboro, KY"
       />
       <InstagramImage
-        src="/images/christineWessa_pensacola_walking-across-street.jpg"
-        alt=""
+        src="/images/insta/IMG_6441.jpeg"
+        alt="Christine Simpson Wessa and Ava at the park, smiling"
       />
     </div>
   );
