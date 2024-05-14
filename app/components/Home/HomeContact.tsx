9276b2755633c9b268c10907cecb03405ea1a60b
Kevin Wessa <kjwessa@gmail.com>
2024-05-14 12:42:23 -0400

    feat(ui): add styling to HomeContact section

--- app/components/Home/HomeContact.tsx
+++ app/components/Home/HomeContact.tsx
@@ -3,10 +3,17 @@ import { ContactForm } from "../ContactForm";
 export function HomeContact() {
   return (
     <section className="border-b-1 border-slate-200 py-24">
-      <div className="px-2">
-        <h2 className="text-4xl">
-          Fill out the form below and I will be in touch within 48 hrs.{" "}
-        </h2>
+      <div className="mx-auto flex max-w-2xl flex-col gap-8 px-4">
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h2 className="text-balance text-4xl">
+            Fill out the form below and I will be in touch within 48 hrs.{" "}
+          </h2>
+          <p className="text-sm text-gray-500">
+            Please complete all required fields, indicated with{" "}
+            <span className="text-red-500">*</span>
+          </p>
+        </div>
+
         <ContactForm />
       </div>
     </section>
