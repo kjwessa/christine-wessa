f1fe8944ec50648c99a825a12f5dad1bc3a8a3e3
Kevin Wessa <kjwessa@gmail.com>
2024-05-15 10:54:06 -0400

    feat(layout): improve HomeHero and HomeMission on mobile devices

--- app/components/Home/HomeHero.tsx
+++ app/components/Home/HomeHero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function HomeHero() {
   return (
     <section className="px-2">
-      <div className="relative flex min-h-[96dvh] flex-col items-center justify-center overflow-hidden rounded-md px-2">
+      <div className="relative flex min-h-[92dvh] flex-col items-center justify-center overflow-hidden rounded-md px-2 sm:min-h-[95dvh] ">
         <Image
           src="/images/christineWessa_pensacola_walking-across-street.jpg"
           className="object-cover"
@@ -12,13 +12,13 @@ export function HomeHero() {
           priority
         />
         <div className="absolute left-0 top-0 z-10 max-w-xl p-4">
-          <p className="text-5xl text-gray-50">
+          <h1 className="max-w-md text-balance text-2xl text-gray-50 sm:text-3xl md:text-4xl lg:text-5xl">
             Hi, I&apos;m Christine Wessa. I&apos;m a speaker & worship leader
             with a message of hope for mothers.
-          </p>
+          </h1>
         </div>
         <div className="absolute bottom-0 ">
-          <p className="whitespace-nowrap text-[14vw] leading-none text-gray-50">
+          <p className="whitespace-nowrap text-[13.5vw] leading-none text-gray-50 sm:text-[14vw]">
             Christine Wessa
           </p>
         </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,18 +5,16 @@ export function Navbar() {
     <header className="px-4 py-3">
       <div className="flex w-full flex-row justify-between">
         <Link href="/">
-          <span className="font-display text-1xl font-bold">
-            Christine Wessa
-          </span>
+          <span className="font-bold">Christine Wessa</span>
         </Link>
         <div className="flex flex-row gap-3">
           <Link href="/#about">
-            <span className="font-display text-1xl font-normal">
+            <span className="text-base font-normal sm:text-base">
               Meet Christine
             </span>
           </Link>
           <Link href="/#contact">
-            <span className="font-display text-1xl font-normal">Contact</span>
+            <span className="text-base font-normal sm:text-base">Contact</span>
           </Link>
         </div>
       </div>
