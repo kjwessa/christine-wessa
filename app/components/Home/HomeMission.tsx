587ac5c589d0845d3f6f55bb1aeb16a0c3510fce
Kevin Wessa <kjwessa@gmail.com>
2024-05-29 10:50:01 -0400

    chores(ui): remove placeholder images on HomeMission

--- app/components/Home/HomeMission.tsx
+++ app/components/Home/HomeMission.tsx
@@ -1,17 +1,11 @@
 import { Eyebrow } from "../Eyebrow";
-import { ImageStamp } from "../ImageStamp";
 
 export function HomeMission() {
   return (
     <section className="border-gray-200-200 border-b-1">
       <div className="mx-auto grid max-w-6xl grid-cols-1 items-center gap-2 px-2 py-16 md:grid-cols-[1fr_auto_1fr] md:py-20 lg:py-24">
         <div className="col-start-1 hidden w-full md:block">
-          <div className="m-auto max-w-[11rem] md:w-3/4">
-            <ImageStamp
-              src="/images/christineWessa_pensacola_walking-across-street.jpg"
-              alt=""
-            />
-          </div>
+          <div className="m-auto max-w-[11rem] md:w-3/4"></div>
         </div>
         <div className="px-2text-center mx-auto flex max-w-lg flex-col items-center justify-center gap-4 text-balance sm:gap-3 md:col-start-2 md:max-w-lg">
           <Eyebrow>My Mission</Eyebrow>
@@ -21,12 +15,7 @@ export function HomeMission() {
           </p>
         </div>
         <div className="hidden w-full md:block">
-          <div className="mx-auto max-w-[11rem] md:w-3/4">
-            <ImageStamp
-              src="/images/christineWessa_pensacola_walking-across-street.jpg"
-              alt=""
-            />
-          </div>
+          <div className="mx-auto max-w-[11rem] md:w-3/4"></div>
         </div>
       </div>
     </section>
