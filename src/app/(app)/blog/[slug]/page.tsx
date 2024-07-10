398b02c9c0a2dab890b6ad4522474b19a9796144
Kevin Wessa <kjwessa@gmail.com>
2024-07-09 21:06:12 -0400

    feat(ui): add Content Collections to individual /blog/individual page

--- src/app/(app)/blog/[slug]/page.tsx
+++ src/app/(app)/blog/[slug]/page.tsx
@@ -1,7 +1,26 @@
 import { allPosts } from "content-collections";
-console.log(allPosts);
-
+import { MDXContent } from "@content-collections/mdx/react";
+import { notFound } from "next/navigation";
 //* Build the individual blog page
-export default async function Page() {
-  return <article></article>;
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => {
+    slug: post.slug;
+  });
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const post = allPosts.find((post) => post.slug === params.slug);
+  if (!post) {
+    notFound();
+  }
+  return (
+    <article>
+      <h1 className="mb-12 text-balance text-5xl font-medium">{post.title}</h1>
+
+      <div className="prose prose-lg mx-auto pb-24 pt-12">
+        <MDXContent code={post.mdx} />
+      </div>
+    </article>
+  );
 }
