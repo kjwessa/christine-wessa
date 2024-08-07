4ad262b32a430510e1f478ff9eb2e129811ab798
Kevin Wessa <kjwessa@gmail.com>
2024-08-06 20:14:10 -0400

    chore(deps): remove Million JS

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-import MillionLint from "@million/lint";
 import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// Apply MillionLint first
-const withMillionLint = MillionLint.next({
-  rsc: true,
-});
-
-// Export the configuration with Content Collections as the last plugin
-export default withContentCollections(withMillionLint(nextConfig));
+// Export the configuration with Content Collections
+export default withContentCollections(nextConfig);
