0a913f890689cc968569faeb2d870cfd2f810135
Kevin Wessa <kjwessa@gmail.com>
2024-08-09 16:42:49 -0400

    chore(code): hide Content Collection code during removal and rebuild

--- src/app/(frontend)/blog/page.tsx
+++ src/app/(frontend)/blog/page.tsx
@@ -1,4 +1,4 @@
-import { allPosts } from "content-collections";
+// import { allPosts } from "content-collections";
 import { BlogCard } from "@/app/components/BlogCard";
 
 export default function Blog() {
@@ -8,17 +8,7 @@ export default function Blog() {
         <div className="mb-12 flex items-center justify-between text-[2.13rem] leading-9">
           <h3>Latest News and Views</h3>
         </div>
-        <div>
-          {allPosts.map((post) => (
-            <BlogCard
-              key={post.title}
-              title={post.title}
-              publishedAt={post.publishedAt}
-              description={post.description}
-              slug={post.slug}
-            />
-          ))}
-        </div>
+        <div></div>
       </div>
     </section>
   );
